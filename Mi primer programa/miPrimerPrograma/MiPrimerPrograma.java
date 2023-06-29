d1869a95340aa6ca3ac2cf6ce827d914d942cbfd
Sebastian Gonzalez <sebastian.javier.gon@gmail.com>
2023-06-29 10:19:20 -0400

    Mi Primer Programa
    Actividad "Mi Primer Programa" de Java Fundamentals

---
+++ Mi primer programa/miPrimerPrograma/MiPrimerPrograma.java
@@ -0,0 +1,13 @@
+package miPrimerPrograma;
+
+public class MiPrimerPrograma {
+	
+	public void miPrimerPrograma(String linea1, String linea2, String linea3) {
+		
+		System.out.println(linea1);
+		
+		System.out.println(linea2);
+		
+		System.out.println(linea3);
+	}
+}
---
+++ Mi primer programa/miPrimerPrograma/MiPrimerProgramaTest.java
@@ -0,0 +1,14 @@
+package miPrimerPrograma;
+
+public class MiPrimerProgramaTest {
+
+	public static void main(String[] args) {
+		
+		
+	       MiPrimerPrograma x = new MiPrimerPrograma();
+	       
+	       x.miPrimerPrograma("Mi nombre es Coding Dojo", "Tengo 100 años de edad", "Mi ciudad es Burbank, CA");
+
+	}
+
+}
